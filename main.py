2a0678782f7b0483c4b4901dd9008495cb542641
Sandi_Navoiy <santes.au@gmail.com>
2025-04-02 23:25:50 +0300

    READMI

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from scr.operations import get_portfolio, get_positions
 from scr.users import account_id
 
 if __name__ == "__main__":
-    id = str(account_id())
+    print("Введите 1-11")
+    print("2 - Управление инвестиционным портфелем")
+    print("3 - Аналитика и уведомления")
+    print("4 - Сравнение активов")
+    print("5 - Информация об инструментах")
+    print("6 - Фильтрация и поиск активов")
+    print("7 - Экспорт данных")
+    print("8 - Технический анализ")
+    print("9 - Симуляция инвестиций")
+    print("10 - Новости и события")
+    print("11 - Интеграция с брокером (опционально, если есть доступ)")
 
-    pprint(get_positions(id))
+
+
